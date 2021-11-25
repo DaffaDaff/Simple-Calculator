e1fa4ed7a314ff81466a25af40a323ed0d7e87ce
Sulthan Daffa <94991158+DaffaDaff@users.noreply.github.com>
2021-11-26 05:26:45 +0700

    Add Input & Start Function

--- SimpleCalculatorV2.c
+++ SimpleCalculatorV2.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+char str[500];
+int lenght;
+
+int start()
+{
+    printf("Enter Operation: \n");
+    scanf("%[^\n]s", str);
+    
+    lenght = strlen(str);
+    printf("%d", lenght);
+}
 
 int main()
 {
-  return 0;
+    start();
+    
+    return 0;
 }
