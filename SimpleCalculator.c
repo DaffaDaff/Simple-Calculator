b598d96232ff445c3740d996ce0c7dd0a9446bd4
Sulthan Daffa <94991158+DaffaDaff@users.noreply.github.com>
2021-11-25 12:42:35 +0700

    Add string input & get string length function

--- SimpleCalculator.c
+++ SimpleCalculator.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+
+// Function to get length of a string
+// from start to end of char
+int getLength(char str[])
+{
+    for(int i = 0; i < 500; i++)
+    {
+        if(str[i] == 0) 
+        {
+            return i;
+        }
+    }
+}
+
 int main()
 {
-  printf("Hello World!");
-  
-  return 0;
+    char str[500];
+    int len;
+    
+    scanf("%s", str);
+    
+    len = getLength(str);
+    
+    
+    
+    return 0;
 }
+
